0c2c71117111985c9d67e89e3ccb309818277f4e
masakihn <77991686+masakihn@users.noreply.github.com>
2021-09-27 00:16:26 -0500

    To do: see if this can be a fun prototype

--- proto1/enemy.gd
+++ proto1/enemy.gd
@@ -3,6 +3,6 @@ func _ready():
 	randomize()
 	
 func _on_Timer_timeout():
-	$AnimationPlayer.play("ArmatureAction")
-	$Timer.set_wait_time(randi() % 3)
+	$AnimationPlayer.play("Attack")
+	$Timer.set_wait_time(randi() % 5)
 	$Timer.start()
